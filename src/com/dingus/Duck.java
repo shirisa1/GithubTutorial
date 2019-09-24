60ac825f72e898714baa643e25f109a53be4aa4b
sach <sach@192.168.1.48>
2019-09-24 15:24:01 -0500

    New Changes to Code

--- src/com/dingus/Duck.java
+++ src/com/dingus/Duck.java
@@ -1,8 +1,22 @@
 package com.dingus;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Duck {
 	public static void main(String[] args) {
 		System.out.println("Dingus Ducks Dingus");
+		List<String> list = new ArrayList<String>();
+		list.add("Barack Obama");
+		list.add("Michelle Obama");
+		list.add("Sasha Obama");
+		list.add("Malia Obama");
+		
+		
+		for (String string : list) {
+			System.out.println(string);
+		}
+		
 	}
 
 }
